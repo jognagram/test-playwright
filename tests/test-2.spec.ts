7cfbbc5f320d6d73036bc9da79aff7f782ff06e1
jogna <jordan.gnamien@smile.ci>
2023-08-02 17:00:38 +0000

    auto-commit

--- tests/test-2.spec.ts
+++ tests/test-2.spec.ts
@@ -4,7 +4,7 @@ test('Ensure we can login and logout', async ({ page }) => {
   await page.goto('https://demo.filamentphp.com/login');
   await page.getByLabel('Email address*').fill('admin@filamentphp.com');
   await page.getByLabel('Password*').fill('password');
-  await page.getByRole('button', { name: 'Login' }).click();
+  await page.getByRole('button').click();
   await page.waitForSelector('body');
   await expect(page).toHaveURL('https://demo.filamentphp.com/admin/dashboard');
   await page.getByRole('button', { name: 'Account' }).click();
