2b6b891f8108ea7c9ff881210b3567a6a93eec77
jogna <jordan.gnamien@smile.ci>
2023-08-02 16:55:58 +0000

    auto-commit

--- tests/test-2.spec.ts
+++ tests/test-2.spec.ts
@@ -1,5 +1,16 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
-  // Recording...
+test('Ensure we can login and logout', async ({ page }) => {
+  await page.goto('https://demo.filamentphp.com/login');
+  await page.getByLabel('Email address*').fill('admin@filamentphp.com');
+  await page.getByLabel('Password*').fill('password');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await page.waitForSelector('body');
+  await expect(page).toHaveURL('https://demo.filamentphp.com/admin/dashboard');
+  await page.getByRole('button', { name: 'Account' }).click();
+  await page.getByRole('menuitem', { name: 'Logout' }).click();
+  await page.waitForSelector('body');
+  await expect(page).toHaveURL('https://demo.filamentphp.com/login');
+
+
 });
