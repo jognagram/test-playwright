06f00affe3437131be0603f55de07841d5b125e3
jogna <jordan.gnamien@smile.ci>
2023-08-02 10:19:26 +0000

    record

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,8 +1,10 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  await page.locator('body').click();
+  // await page.locator('body').click();
   await page.goto('https://demo.playwright.dev/todomvc/#/');
+  await page.waitForSelector('body');
+  await page.locator('body').click();
   await page.getByPlaceholder('What needs to be done?').fill('feed the dog');
   await page.getByPlaceholder('What needs to be done?').press('Enter');
   await page.getByPlaceholder('What needs to be done?').fill('water the plants');
