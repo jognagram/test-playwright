83c2e1504bff242b53ba102d18e917dc5023ccef
jogna <jordan.gnamien@smile.ci>
2023-08-02 10:02:55 +0000

    auto-commit

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -7,15 +7,17 @@ test('test', async ({ page }) => {
   await page.getByPlaceholder('What needs to be done?').press('Enter');
   await page.getByPlaceholder('What needs to be done?').fill('water the plants');
   await page.getByPlaceholder('What needs to be done?').press('Enter');
-  
-  await expect(page.getByTestId('todo-title')).toHaveCount(2);
+  await page.getByPlaceholder('What needs to be done?').click();
+  await page.getByPlaceholder('What needs to be done?').fill('do the shopping');
+  await page.getByPlaceholder('What needs to be done?').press('Enter');
+  await expect(page.getByTestId('todo-title')).toHaveCount(3);
   await page
     .locator('li')
     .filter({ hasText: 'feed the dog' })
     .getByLabel('Toggle Todo')
     .check();
   await page.getByRole('link', { name: 'Active' }).click();
-  await expect(page.getByTestId('todo-title')).toHaveCount(1);
+  await expect(page.getByTestId('todo-title')).toHaveCount(2);
   await page.getByRole('link', { name: 'Completed' }).click();
   await expect(page.getByTestId('todo-title')).toHaveCount(1);
 });
