e7d1498e6a7e1f91f07bc1c30e98a708ede082cd
jogna <jordan.gnamien@smile.ci>
2023-08-08 16:49:12 +0000

    auto-commit

--- tests/wikepedia.test.ts
+++ tests/wikepedia.test.ts
@@ -2,9 +2,14 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('https://www.wikipedia.org/');
-  await page.locator('strong:has-text("Français")').click();
-  await page.locator('[placeholder="Rechercher sur Wikipédia"]').click();
-  await page.keyboard.type('raton laveur');
+  // await page.locator('strong:has-text("Français")').click();
+  // await page.locator('[placeholder="Rechercher sur Wikipédia"]').click();
+  // await page.keyboard.type('raton laveur');
+  // await page.keyboard.press('Enter');
+  // await expect(page.locator('h1')).toHaveText('/Raton laveur');
+  await page.click('text=Français');
+  await page.fill('[paceholder="Rechercher sur Wikipédia"]', 'raton laveur');
   await page.keyboard.press('Enter');
-  await expect(page.locator('title')).toHaveText("Raton laveur");
+  await page.waitForSelector('text=Raton laveur');
+  await expect(page.locator('h1')).toHaveText(/Raton laveur/);
 });
